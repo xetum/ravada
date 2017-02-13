02c5eea8e86583680a0ff441c65b10dcb01425f3
Francesc Guasch <frankie@etsetb.upc.edu>
2017-02-13 12:54:26 +0100

    [#110] Ubuntu mini remix xml and data

---
+++ sql/sqlite/insert_iso_miniremix.sql
@@ -0,0 +1,9 @@
+INSERT INTO iso_images 
+    (id,name,description,arch,xml,xml_volume,url,md5)
+    VALUES (35,'Ubuntu Mini Remix'
+    ,'Ubuntu Mini Remix 16.04 32 bits'
+    ,'i386'
+    ,'miniremix-16.04-i386.xml'
+    ,'miniremix-volume.xml'
+    ,'http://ubuntu-mini-remix.mirror.garr.it/mirrors/ubuntu-mini-remix/16.04/ubuntu-mini-remix-16.04-i386.iso'
+    ,'303abd1dd70907bf904174022818abe7');
