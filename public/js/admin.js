0d6705112a193ca810162efe825c0c5fdb020d36
fv3rdugo <fv3rdugo@gmail.com>
2017-04-20 17:49:31 +0200

    Polish names

--- public/js/admin.js
+++ public/js/admin.js
@@ -174,6 +174,6 @@ ravadaApp.directive("solShowMachine", swMach)
     $scope.updatePromise = $interval($scope.updateMessages,3000);
   };
 
-angular.module('DemoApp', ['ngMessages']);
+angular.module('Netform', ['ngMessages']);
 
-angular.bootstrap(document.getElementById('demoApp'), ['DemoApp']);
+angular.bootstrap(document.getElementById('netform'), ['Netform']);
